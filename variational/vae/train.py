5307f380d532b1fbeb001d14c050301b3d6320af
Edgar Ortiz <ed.ortizm@gmail.com>
2021-10-27 11:44:49 -0300

    variational autoencoder working with processed data

--- variational/vae/train.py
+++ variational/vae/train.py
@@ -7,13 +7,13 @@ import time
 ###############################################################################
 import numpy as np
 
-from autoencoders.variational.autoencoder import VAE, Encoder
+from autoencoders.variational.autoencoder import VAE
 ###############################################################################
 ti = time.time()
 ###############################################################################
 parser = ConfigParser(interpolation=ExtendedInterpolation())
 parser.read("vae.ini")
-################################################################################
+###############################################################################
 # load data
 data_directory = parser.get("directories", "train")
 data_name = parser.get("files", "train")
@@ -24,25 +24,23 @@ architecture = dict(parser.items("architecture"))
 architecture["input_dimensions"] = input_dimensions
 
 hyperparameters = dict(parser.items("hyperparameters"))
-encoder = Encoder(architecture)
 ###############################################################################
-# vae = VAE(
-#     architecture,
-#     hyperparameters,
-# )
-#
-# vae.summary()
-# ################################################################################
-# # Training the model
-# history = vae.train(data)
-# # save model
-# models_directory = parser.get("directories", "models")
-#
-# if not os.path.exists(f"{models_directory}/{vae.architecture_str}"):
-#     os.makedirs(f"{models_directory}/{vae.architecture_str}")
-#
-# vae.save_model(models_directory)
-###############################################################################
+vae = VAE(
+    architecture,
+    hyperparameters,
+)
+
+vae.summary()
+#############################################################################
+# Training the model
+history = vae.train(data)
+# save model
+models_directory = parser.get("directories", "models")
+
+if not os.path.exists(f"{models_directory}/{vae.architecture_str}"):
+    os.makedirs(f"{models_directory}/{vae.architecture_str}")
+
+vae.save_model(models_directory)
 ###############################################################################
 tf = time.time()
 print(f"Running time: {tf-ti:.2f}")
