e976c8a14a7ef3e9d970a7d0cfabc33bc254f959
Edgar Ortiz <ed.ortizm@gmail.com>
2021-11-05 15:06:48 -0300

    Fix output directory for trained vae

--- variational/vae/train.py
+++ variational/vae/train.py
@@ -8,6 +8,7 @@ import time
 import numpy as np
 
 from autoencoders.variational.autoencoder import VAE
+from sdss.superclasses import FileDirectory
 ###############################################################################
 ti = time.time()
 ###############################################################################
@@ -19,14 +20,6 @@ print(f"Load data")
 data_directory = parser.get("directories", "train")
 data_name = parser.get("files", "train")
 data = np.load(f"{data_directory}/{data_name}")
-
-#nan_values = np.count_nonzero(np.isnan(data))
-#indefinite_values = np.count_nonzero(~np.isfinite(data))
-#print(f"Nans and indefinte: {nan_values, indefinite_values}")
-
-# Shuffle for better performance in SGD
-#print(f"Shuffle in place train data")
-# np.random.shuffle(data)
 input_dimensions = data.shape[1]
 ###############################################################################
 architecture = dict(parser.items("architecture"))
@@ -46,12 +39,11 @@ vae.summary()
 # Training the model
 history = vae.train(data)
 # save model
-models_directory = parser.get("directories", "models")
+model_directory = parser.get("directories", "output")
+model_directory = f"{model_directory}/{vae.architecture_str}"
+FileDirectory().check_directory(model_directory, exit=False)
 
-if not os.path.exists(f"{models_directory}/{vae.architecture_str}"):
-    os.makedirs(f"{models_directory}/{vae.architecture_str}")
-
-vae.save_model(models_directory)
+vae.save_model(model_directory)
 ###############################################################################
 tf = time.time()
 print(f"Running time: {tf-ti:.2f}")
