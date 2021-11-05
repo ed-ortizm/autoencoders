b084ff66545cf00ea256f863792d0d441576ab28
Edgar Ortiz <ed.ortizm@gmail.com>
2021-11-05 15:15:05 -0300

    Set directory to save trained model (ae)

--- deterministic/ae/train.py
+++ deterministic/ae/train.py
@@ -4,10 +4,10 @@ import os
 import sys
 import time
 
-###############################################################################
 import numpy as np
 
 from autoencoders.deterministic.autoencoder import AE
+from sdss.superclasses import FileDirectory
 ###############################################################################
 ti = time.time()
 ###############################################################################
@@ -19,14 +19,6 @@ print(f"Load data")
 data_directory = parser.get("directories", "train")
 data_name = parser.get("files", "train")
 data = np.load(f"{data_directory}/{data_name}")
-
-#nan_values = np.count_nonzero(np.isnan(data))
-#indefinite_values = np.count_nonzero(~np.isfinite(data))
-#print(f"Nans and indefinte: {nan_values, indefinite_values}")
-
-# Shuffle for better performance in SGD
-#print(f"Shuffle in place train data")
-# np.random.shuffle(data)
 input_dimensions = data.shape[1]
 ###############################################################################
 architecture = dict(parser.items("architecture"))
@@ -47,12 +39,11 @@ ae.summary()
 # Training the model
 history = ae.train(data)
 # save model
-models_directory = parser.get("directories", "models")
+model_directory = parser.get("directories", "output")
+# model_directory = f"{model_directory}/{ae.architecture_str}"
+FileDirectory().check_directory(model_directory, exit=False)
 
-if not os.path.exists(f"{models_directory}/{ae.architecture_str}"):
-    os.makedirs(f"{models_directory}/{ae.architecture_str}")
-
-ae.save_model(models_directory)
+ae.save_model(model_directory)
 ###############################################################################
 tf = time.time()
 print(f"Running time: {tf-ti:.2f}")
