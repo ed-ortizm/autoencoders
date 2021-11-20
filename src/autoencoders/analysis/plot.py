0ae768bd8fc8cf9c8ab74d1cb1553308a5d2e74b
Edgar Ortiz <ed.ortizm@gmail.com>
2021-11-20 10:17:08 -0300

    Add plot_train_history method for VAEs

--- src/autoencoders/analysis/plot.py
+++ src/autoencoders/analysis/plot.py
@@ -1,22 +1,59 @@
-################################################################################
-def plot_history(history: "tf.keras.callback.History", save_to: "str"):
+import matplotlib.pyplot as plt
+import numpy as np
+###############################################################################
 
-    for key, value in history.history.items():
+def plot_train_history(train_history: dict, kl_weight: np.float, save_to: str):
 
-        # fig, ax = plt.subplots(figsize=(10, 5))
+    """
+    Plot loss, regularization and reconstruction
 
-        # ax.plot(value)
+    PARAMETERS
+        train_history: {
 
-        # ax.set_title(f'Model {key}')
-        # ax.set_xlabel(f'epochs')
-        # ax.set_ylabel(f'{key}')
+        "parameters": {
+                'batch_size': 256,
+                'epochs': 10,
+                'steps': None,
+                'samples': 328479,
+                'verbose': 1,
+                'do_validation': False,
+                'metrics': ['loss', 'mse']
+            }
 
-        # fig.savefig(f'{save_to}_{key}.png')
-        # plt.close()
+        "history": {
+            'loss': [0.39, ..., 0.12339567896077754],
+            'mse': [0.38579124, ..., 0.11851482]
+            }
+            this key contains the loss function during training,
+            the other metrics shown, correspond to the ones used
+            to monitor the training
+            # Add custom metrics to show the rest of parameters
 
-        np.save(f"{save_to}_{key}.npy", value)
+        }
 
+    """
 
-################################################################################
-def digits_plot():
-    pass
+    loss = np.array(train_history["history"]["loss"])
+    reconstruction = np.array(train_history["history"]["mse"])
+    kl_divergence = (loss - reconstruction) / kl_weight
+
+    fig, axs = plt.subplots(
+        nrows=2 , ncols=1,
+        figsize=(8, 5),
+        sharex=True
+    )
+
+    axs[0].plot(loss, label="loss")
+    axs[0].plot(reconstruction, label="MSE")
+    axs[0].legend()
+
+    axs[1].plot(kl_divergence, label="KL-divergence")
+    axs[1].legend()
+
+    axs[1].set_xlabel(f'epochs')
+
+    fig.savefig(f'{save_to}.pdf')
+
+    plt.close()
+
+###############################################################################
