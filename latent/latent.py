a7a61a743b3fabd3cae0759d869a45c55c894598
Edgar Ortiz <ed.ortiz@gmail.com>
2022-04-18 05:28:07 -0400

    feature: implement script to get latent representation of data

--- latent/latent.py
+++ latent/latent.py
@@ -31,11 +31,6 @@ parser = ConfigParser(interpolation=ExtendedInterpolation())
 config_file_name = "latent.ini"
 parser.read(f"{config_file_name}")
 ###############################################################################
-# Check files and directory
-check = FileDirectory()
-# Handle configuration file
-configuration = ConfigurationFile()
-###############################################################################
 # set the number of cores to use per model in each worker
 jobs = parser.getint("configuration", "cores_per_worker")
 config = tf.compat.v1.ConfigProto(
@@ -61,19 +56,27 @@ model = AutoEncoder(reload=True, reload_from=model_directory)
 print(f"Load data", end="\n")
 
 data_directory = parser.get("directory", "data")
-fluxes_name = parser.get("file","fluxes")
+FileDirectory().check_directory(data_directory, exit=True)
 
+fluxes_name = parser.get("file","fluxes")
 fluxes = np.load(f"{data_directory}/{fluxes_name}")
 ###############################################################################
 print(f"Compute and save latent representation of data", end="\n")
+
 latent_representation = model.encode(fluxes)
-np.save("/home/edgar/latent.npy", latent_representation)
+
+save_data_to = parser.get("directory", "latent")
+save_data_to = f"{save_data_to}/{model_id}"
+FileDirectory().check_directory(save_data_to, exit=False)
+
+np.save(f"{save_data_to}/latent_{bin_id}.npy", latent_representation)
 ###############################################################################
 session.close()
 ###############################################################################
-# save_config_to = f"{data_directory}/"
-# with open(f"{save_config_to}/{config_file_name}", "w") as config_file:
-#     parser.write(config_file)
+print(f"Save configuration files of run", end="\n")
+
+with open(f"{save_data_to}/{config_file_name}", "w") as config_file:
+    parser.write(config_file)
 ###############################################################################
 finish_time = time.time()
 print(f"\nRun time: {finish_time - start_time:.2f}")
