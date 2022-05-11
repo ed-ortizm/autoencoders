2f62501d244b0970f7442cad3f6ca65d514aaf26
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-05-10 21:59:10 -0400

    feature: implement script to get 2D view of lantent space with UMAP

--- latent/umapvisual.py
+++ latent/umapvisual.py
@@ -22,24 +22,31 @@ print(f"Load data", end="\n")
 
 latent_directory = parser.get("directory", "latent")
 latent_name = parser.get("file", "latent")
-print(f"{latent_directory}/{latent_name}")
 FileDirectory().file_exists(
     f"{latent_directory}/{latent_name}",
     exit_program=True
 )
 
+latent = np.load(f"{latent_directory}/{latent_name}")
 ###############################################################################
-print(f"Define UMAP reducers", end="\n")
-umap_paramenters = config.section_to_dictionary(
-    parser.items("umap"), [","]
-)
-print(umap_paramenters)
-reducer = umap.UMAP()
+metrics = config.entry_to_list(parser.get("umap", "metrics"), str, ",")
+
+bin_id = parser.get("common", "bin")
+
+for metric in metrics:
+
+    print(f"UMAP with {metric} metric", end="\n")
+
+    reducer = umap.UMAP(metric=metric)
+    embedding = reducer.fit_transform(latent)
+
+    np.save(f"{latent_directory}/umap_{metric}_{bin_id}.npy", embedding)
+
 ###############################################################################
-# print(f"Save configuration files of run", end="\n")
-# save_to = "."
-# with open(f"{save_to}/{config_file_name}", "w") as config_file:
-#     parser.write(config_file)
+print(f"Save configuration file", end="\n")
+
+with open(f"{latent_directory}/{config_file_name}", "w") as config_file:
+    parser.write(config_file)
 ###############################################################################
 finish_time = time.time()
 print(f"\nRun time: {finish_time - start_time:.2f}")
