572ab9773492cd48b1389cf76c59009903dd9869
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-05-11 21:56:38 -0400

    feature: load latent and umap embeddings to bin data frame to get visuals

--- latent/visual_latent.py
+++ latent/visual_latent.py
@@ -19,15 +19,16 @@ config = ConfigurationFile()
 print(f"Load metadata", end="\n")
 data_directory = parser.get("directory", "data")
 science_df = parser.get("file", "science")
-
 science_df = pd.read_csv(
     f"{data_directory}/{science_df}", index_col="specobjid"
 )
 
 bin_directory = parser.get("directory", "bin_data")
 specobjid_name = parser.get("file", "specobjid")
-
 specobjid = np.load(f"{bin_directory}/{specobjid_name}")
+
+bin_df = science_df.loc[specobjid[:, 1]]
+del science_df
 ###############################################################################
 print(f"Load latent representation", end="\n")
 
@@ -40,10 +41,25 @@ FileDirectory().file_exists(
 )
 
 latent = np.load(f"{latent_directory}/{latent_name}")
-###############################################################################
-metrics = config.entry_to_list(parser.get("umap", "metrics"), str, ",")
+number_variables = latent.shape[1]
 
+for idx in range(number_variables):
+    bin_df[f"{idx:02d}Latent"] = latent[:, idx]
+
+###############################################################################
+print(f"Load umap embedding", end="\n")
+metrics = config.entry_to_list(parser.get("umap", "metrics"), str, ",")
 bin_id = parser.get("common", "bin")
+
+for metric in metrics:
+
+    embedding = np.load(f"{latent_directory}/umap_{metric}_{bin_id}.npy")
+
+    bin_df[f"{metric}_01"] = embedding[:, 0]
+    bin_df[f"{metric}_02"] = embedding[:, 1]
+
+
+print(bin_df.columns, bin_df.shape)
 ###############################################################################
 print(f"Save configuration file", end="\n")
 
