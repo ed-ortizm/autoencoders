dfee73f3e0ce9cd68fc9c418a0058b23148384c6
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-05-16 15:54:26 -0400

    feature: script to get umap visual and pair plots #SeparationOfConcerns

--- latent/umap_visual.py
+++ latent/umap_visual.py
@@ -75,12 +75,7 @@ models_ids = [model_id.split("/")[-2] for model_id in latent_directories]
 
 for model_idx, latent_directory in latent_directories:
 
-    latent = np.load(f"{latent_directory}/{latent_name}")
-    number_latent_variables = latent.shape[1]
-
-    # load latent representation to data frame
-    for idx in range(number_latent_variables):
-        bin_df[f"{idx:02d}Latent"] = latent[:, idx]
+    print(f"model {models_ids[model_idx]}: UMAP", end="\n")
 
     # load umap embedding
     for metric in metrics:
@@ -90,51 +85,14 @@ for model_idx, latent_directory in latent_directories:
         bin_df[f"{metric}_01"] = embedding[:, 0]
         bin_df[f"{metric}_02"] = embedding[:, 1]
 
-    print(f"model {models_ids[model_idx]}: pair plots", end="\n")
 
     for hue in parameters_of_plot["hues"]:
 
         bin_df_of_plot = bin_df[bin_df[hue] != "undefined"]
 
-        for latent_x in range(number_latent_variables):
-
-            for latent_y in range(latent_x, number_latent_variables):
-
-                if latent_x == latent_y:
-                    continue
-
-                print(
-                    f"Pair plots: {latent_x:02d} vs {latent_y:02d}", end="\r"
-                )
-
-                # pair_plot = sns.scatterplot(
-                sns.scatterplot(
-                    x=f"{latent_x:02d}Latent",
-                    y=f"{latent_y:02d}Latent",
-                    ax=ax,
-                    data=bin_df_of_plot,
-                    hue=hue,
-                    alpha=parameters_of_plot["alpha"],
-                    marker_size = parameters_of_plot["marker_size"],
-                    edgecolors = parameters_of_plot["edgecolors"],
-                )
-
-                save_to = f"{latent_directory}/pair_plots"
-                manage_files.check_directory(save_to, exit_program=False)
-
-                fig.savefig(
-                    f"{save_to}/"
-                    f"pair_{latent_x:02d}_{latent_y:02d}_"
-                    f"{hue}.{parameters_of_plot['format']}"
-                )
-
-                ax.clear()
-        #######################################################################
-        print(f"model {models_ids[model_idx]}: UMAP", end="\n")
-
         for metric in metrics:
 
-            print(f"UMAP metric: {metric}", end="\r")
+            print(f"UMAP metric: {metric}. Hue: {hue}", end="\r")
 
             pair_plot = sns.scatterplot(
                 x=f"{metric}_01",
